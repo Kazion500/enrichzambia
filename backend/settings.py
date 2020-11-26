41fb33bdece4fad9c4d5b8b76e2fb9796630b9da
Kazion500 <patrickckabwe@gmail.com>
2020-11-26 17:43:18 +0200

    added static root to the main urls

--- backend/settings.py
+++ backend/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-# else:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'enrichdb',
-#         'USER': 'kazionenrich',
-#         'PASSWORD': 'Changepass1',
-#         'HOST': 'localhost',
-#         'PORT': '',
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
 #     }
-# }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'enrichdb',
+        'USER': 'kazionenrich',
+        'PASSWORD': 'Changepass1',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
